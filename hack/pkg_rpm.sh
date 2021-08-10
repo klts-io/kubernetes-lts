fad4b4d06adb88493318d96a4a0c32f057b598d6
Shiming Zhang <wzshiming@foxmail.com>
2021-08-10 10:40:50 +0800

    Support repo for rpm

--- hack/pkg_rpm.sh
+++ hack/pkg_rpm.sh
@@ -7,40 +7,16 @@ set -o pipefail
 source "$(dirname "${BASH_SOURCE}")/helper.sh"
 
 NAMES="${1:-kubelet}"
-ARCHS=${2:-amd64}
+ARCHS="${2:-amd64}"
 VERSION=$(helper::workdir::version)
 RELEASE="${VERSION##*-}"
 VERSION="${VERSION%-*}"
 
-IFS=, NAMES=(${NAMES})
-IFS=, ARCHS=(${ARCHS})
-
 SRC="${ROOT}/hack/rpm"
 RPMBUILD="${ROOT}/rpmbuild"
+RPMREPO="${REPOSDIR}/rpm"
 BIN_PATH="${WORKDIR}/_output/dockerized/bin"
-SPEC_PATH="${RPMBUILD}/SPECS"
-SRC_PATH="${RPMBUILD}/SOURCES"
-RPM_PATH="${RPMBUILD}/RPMS"
 
 docker build -t rpm-builder "${SRC}"
 
-for arch in ${ARCHS[@]}; do
-    for name in ${NAMES[@]}; do
-        echo "Building RPM's for ${arch}"
-        bin="${BIN_PATH}/linux/${arch}/${name}"
-        src="${SRC_PATH}/${name}-${VERSION}"
-        tar="${SRC_PATH}/${name}-${VERSION}.tar.gz"
-
-        mkdir -p "${src}" "${SPEC_PATH}"
-
-        cp -r "${SRC}/${name}"/* "${src}/"
-        mv "${src}/${name}.spec" "${SPEC_PATH}/${name}.spec"
-        cp "${bin}" "${src}/${name}"
-
-        tar -czvf "${tar}" -C "${SRC_PATH}" "${name}-${VERSION}"
-
-        docker run --rm -v "${RPMBUILD}:/root/rpmbuild/" rpm-builder "${name}" "${VERSION}" "${RELEASE}" "${arch}"
-    done
-done
-
-cp "${RPM_PATH}"/*/*.rpm "${OUTPUT}"
+docker run --rm -v "${SRC}:/root/src" -v "${BIN_PATH}:/root/output/bin" -v "${RPMBUILD}:/root/rpmbuild/" -v "${RPMREPO}:/root/rpmrepo/" rpm-builder "${NAMES}" "${ARCHS}" "${VERSION}" "${RELEASE}"
