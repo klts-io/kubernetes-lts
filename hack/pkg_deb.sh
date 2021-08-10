c158d00242509b5bfb202981e3aa8521707ef4e7
Shiming Zhang <wzshiming@foxmail.com>
2021-08-10 10:48:11 +0800

    Support repo for deb

--- hack/pkg_deb.sh
+++ hack/pkg_deb.sh
@@ -12,29 +12,11 @@ VERSION=$(helper::workdir::version)
 RELEASE="${VERSION##*-}"
 VERSION="${VERSION%-*}"
 
-IFS=, NAMES=(${NAMES})
-IFS=, ARCHS=(${ARCHS})
-
 SRC="${ROOT}/hack/deb"
 DEBBUILD="${ROOT}/debbuild"
+DEBREPO="${REPOSDIR}/deb"
 BIN_PATH="${WORKDIR}/_output/dockerized/bin"
-SRC_PATH="${DEBBUILD}/debs"
 
 docker build -t deb-builder "${SRC}"
 
-for arch in ${ARCHS[@]}; do
-    for name in ${NAMES[@]}; do
-        echo "Building DEB's for ${arch}"
-        bin="${BIN_PATH}/linux/${arch}/${name}"
-        src="${SRC_PATH}/${name}-${VERSION}"
-        tar="${SRC_PATH}/${name}-${VERSION}.tar.gz"
-
-        mkdir -p "${src}"
-        cp -r "${SRC}/${name}"/* "${src}/"
-        cp "${bin}" "${src}/${name}"
-
-        docker run --rm -v "${DEBBUILD}:/root/debbuild/" deb-builder "${name}" "${VERSION}" "${RELEASE}" "${arch}"
-    done
-done
-
-cp "${SRC_PATH}"/*.deb "${OUTPUT}"
+docker run --rm -v "${SRC}:/root/src" -v "${BIN_PATH}:/root/output/bin" -v "${DEBBUILD}:/root/debbuild/" -v "${DEBREPO}:/root/debrepo" deb-builder "${NAMES}" "${ARCHS}" "${VERSION}" "${RELEASE}"
