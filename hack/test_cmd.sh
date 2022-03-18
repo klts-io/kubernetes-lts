9e2e8a113499ba61f0bbbc809657a16e83b53c27
Shiming Zhang <wzshiming@foxmail.com>
2022-03-18 10:33:12 +0800

    Fix ci

--- hack/test_cmd.sh
+++ hack/test_cmd.sh
@@ -10,5 +10,10 @@ cd "${WORKDIR}"
 # Kubeadm was added for testing in 1.19 and later
 for n in {1..5}; do
     echo "+++ Test retry ${n}"
-    ./build/shell.sh -c 'make generated_files kubeadm && mkdir -p _output/local/go/bin/ && cp _output/dockerized/bin/linux/amd64/kubeadm _output/local/go/bin/ && ./hack/install-etcd.sh && PATH=$(pwd)/third_party/etcd:${PATH} make test-cmd' 2>&1 | grep -v -E '^I\w+ ' && exit 0
+    ./build/shell.sh -c '
+make generated_files kubeadm
+mkdir -p _output/local/go/bin/ && cp _output/dockerized/bin/linux/amd64/kubeadm _output/local/go/bin/
+./hack/install-etcd.sh
+PATH=$(pwd)/third_party/etcd:${PATH} make test-cmd
+' 2>&1 | grep -v -E '^I\w+ ' && exit 0
 done
