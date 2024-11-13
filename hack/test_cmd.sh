50769f74546eff40344f83be34e5ce9a71b78de4
Paco Xu <paco.xu@daocloud.io>
2024-11-13 18:31:06 +0800

    format patch update-kube-proxy-iptables.1.18.patch

--- hack/test_cmd.sh
+++ hack/test_cmd.sh
@@ -5,7 +5,9 @@ set -o nounset
 set -o pipefail
 
 source "kit/helper.sh"
-KUBE_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
+export KUBE_ROOT=$(dirname "$(readlink -f "$0")")/../src/github.com/kubernetes/kubernetes/
+export TERM=linux
+echo "KUBE_ROOT directory: $KUBE_ROOT"
 cd "${WORKDIR}"
 
 # Kubeadm was added for testing in 1.19 and later
@@ -16,10 +18,10 @@ for n in {1..5}; do
 # before v1.25
 make generated_files kubeadm
 # after v1.26
-make all -C "${KUBE_ROOT}" WHAT=cmd/kubeadm
+./build/run.sh make all WHAT=cmd/kubeadm
 
 mkdir -p _output/local/go/bin/ && cp _output/dockerized/bin/linux/amd64/kubeadm _output/local/go/bin/
 ./hack/install-etcd.sh
-PATH=$(pwd)/third_party/etcd:${PATH} make test-cmd
+TERM=linux PATH=$(pwd)/third_party/etcd:${PATH} make test-cmd
 ' 2>&1 | grep -v -E '^I\w+ ' && exit 0
 done
