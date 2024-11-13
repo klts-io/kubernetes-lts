01ef815d2325fa68e4528aad873e981040a1bb3f
Paco Xu <paco.xu@daocloud.io>
2024-11-13 18:04:39 +0800

    fix test-cmd kubeadm build

--- hack/test_cmd.sh
+++ hack/test_cmd.sh
@@ -5,13 +5,19 @@ set -o nounset
 set -o pipefail
 
 source "kit/helper.sh"
+KUBE_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
 cd "${WORKDIR}"
 
 # Kubeadm was added for testing in 1.19 and later
 for n in {1..5}; do
     echo "+++ Test retry ${n}"
     ./build/shell.sh -c '
+
+# before v1.25
 make generated_files kubeadm
+# after v1.26
+make all -C "${KUBE_ROOT}" WHAT=cmd/kubeadm
+
 mkdir -p _output/local/go/bin/ && cp _output/dockerized/bin/linux/amd64/kubeadm _output/local/go/bin/
 ./hack/install-etcd.sh
 PATH=$(pwd)/third_party/etcd:${PATH} make test-cmd
