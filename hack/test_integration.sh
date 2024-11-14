8e580f081e07cc7f3c8a6f4a821e364424d2ad83
Paco Xu <paco.xu@daocloud.io>
2024-11-14 17:38:23 +0800

    gen  .make/go-pkgdeps.mk

--- hack/test_integration.sh
+++ hack/test_integration.sh
@@ -5,19 +5,30 @@ set -o nounset
 set -o pipefail
 
 source "kit/helper.sh"
+export KUBE_ROOT=$(dirname "$(readlink -f "$0")")/..
+export TERM=linux
+echo "KUBE_ROOT directory: $KUBE_ROOT"
 cd "${WORKDIR}"
+echo "WORKDIR directory: $WORKDIR"
 
 TMPFILE="${TMPDIR}/test-integration.log"
 
 # Etcd was added for testing in 1.21 and later
 function test-integration() {
     ./build/shell.sh -c '
-./hack/install-etcd.sh
-PATH=$(pwd)/third_party/etcd:${PATH} make test-integration
+# generate .make/go-pkgdeps.mk 
+go install ./cmd/...
+
+# run test-integration
+KUBE_RUN_COPY_OUTPUT=y TERM=linux PATH=$(pwd)/third_party/etcd:${PATH} DBG_CODEGEN=1 make test-integration
 '
 }
 
 test-integration 2>&1 | tee "${TMPFILE}" | grep -v -E '^I\w+ ' && exit 0
+
+echo "-------print TMPFILE:"
+cat "${TMPFILE}"
+echo "-------TMPFILE end."
 
 RETRY_CASES=$(cat "${TMPFILE}" | grep -E '^FAIL\s+k8s.io/kubernetes' | awk '{print $2}' || echo "")
 
